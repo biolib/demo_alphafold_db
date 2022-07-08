0e4891bd7185b5ca33281ffee2de1214f8ae53c8
Laura Sans <60480966+laura-biolib@users.noreply.github.com>
2022-07-08 11:40:50 +0200

    Working on app output

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd alphafold_db/ && ls
-
-echo "Path to latest is  alphafold_db/latest/"
-echo "Path to v2 is alphafold_db/v2/"
+echo -e "Main directory:\n" > output.md
+#cd /home/biolib/alphafold_db/ && ls >> output.md
+##
+#echo -e "Path to latest is  alphafold_db/latest/\n" >> output.md
+#cd /home/biolib/alphafold_db/latest/ && ls >> output.md
+##
+#echo -e "Path to v2 is alphafold_db/v2/\n" >> output.md
+#cd /home/biolib/alphafold_db/v2/ && ls >> output.md
