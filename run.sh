6a61a7cdd278de3c4d717cee3bd6426a17138337
Laura Sans <60480966+laura-biolib@users.noreply.github.com>
2022-07-08 12:20:31 +0200

    Finish app

--- run.sh
+++ run.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-echo -e "Main directory:\n" > output.md
-#cd /home/biolib/alphafold_db/ && ls >> output.md
-##
+#echo -e "Main directory:\n" > output.md
+#cd /home/biolib/alphafold_db/ && ls
+###
 #echo -e "Path to latest is  alphafold_db/latest/\n" >> output.md
-#cd /home/biolib/alphafold_db/latest/ && ls >> output.md
-##
+#cd /home/biolib/alphafold_db/latest/ && ls
+###
 #echo -e "Path to v2 is alphafold_db/v2/\n" >> output.md
-#cd /home/biolib/alphafold_db/v2/ && ls >> output.md
+#cd /home/biolib/alphafold_db/v2/ && ls
+
+cat << EOF >> output.md
+\`\`\`
+Main directory:
+$(cd /home/biolib/alphafold_db/ && ls)
+
+Path to latest is  alphafold_db/latest/
+$(cd /home/biolib/alphafold_db/latest/ && ls)
+
+Path to v2 is alphafold_db/v2/
+$(cd /home/biolib/alphafold_db/v2/ && ls)
+\`\`\`
+EOF
